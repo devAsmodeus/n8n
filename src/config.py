7d30f95c93a13c992b7594a4ab61ae2b40c6c8db
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-08-06 16:11:45 +0300

    add config file

---
+++ src/config.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    SECRET_KEY: str
+
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def db_url(self) -> str:
+        return (
+            "postgresql+asyncpg://"
+            f"{self.DB_USER}:"
+            f"{self.DB_PASS}@"
+            f"{self.DB_HOST}:"
+            f"{self.DB_PORT}/"
+            f"{self.DB_NAME}"
+        )
+
+    model_config = SettingsConfigDict(
+        env_file=Path(__file__).parent.parent / ".env"
+    )
+
+
+settings = Settings()
